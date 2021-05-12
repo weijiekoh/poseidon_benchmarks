779a3746ccf5a896c9e1411ab4795dd10b5fd3cf
Koh Wei Jie <contact@kohweijie.com>
2021-05-12 11:19:41 +0100

    fixed circuit compilation and use hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,18 +12,12 @@ module.exports = {
       enabled: true,
       runs: 200
     }
-
   },
-  defaultNetwork: 'localhost',
   networks: {
-    localhost: {
-      url: 'http://localhost:8545',
-      accounts: [
-        '0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e'
-      ],
-      //accounts: {
-        //mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
-      //},
+    hardhat: {
+      accounts: {
+        mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
+      },
       loggingEnabled: false,
       allowUnlimitedContractSize: false
     }
